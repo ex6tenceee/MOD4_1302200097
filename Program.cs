da2ba606ad55acdf76e11ac58fe089b210036ffb
DAFFA AQILAH <daffaqillahhh@365.telkomuniversity.ac.id>
2022-03-19 10:31:36 +0700

    Menambahkan Class Penjumlahan dengan method jumlahtigaangka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Modul4_1302200097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.WriteLine(Penjumlahan.Jumlahkan(13,02,20));
+        }
+    }
+
+    class Penjumlahan
+    {
+        public static void JumlahTigaAngka<T>(T inputUser)
+        {
+            Console.WriteLine("Masukkan tiga angka: " + inputUser);
+        }
+
+        public static int Jumlahkan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
+
+
